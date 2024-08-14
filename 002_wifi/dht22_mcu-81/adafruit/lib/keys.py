63fe61c1396368c0a58354b5bb6cf62f193b4d77
La svedese <5554930+sarasjodin@users.noreply.github.com>
2024-08-14 12:38:38 +0200

    Create keys.py

---
+++ 002_wifi/dht22_mcu-81/adafruit/lib/keys.py
@@ -0,0 +1,4 @@
+AIO_USER = "Change_to_Your_AIO_USER"
+AIO_KEY = "Change_to_Your_AIO_KEY"  # This key should be your Adafruit IO API key, not a feed-specific key or a dashboard key..
+WIFI_SSID = 'Change_to_Your_SSID'
+WIFI_PASS = 'Change_to_Your_WiFi_Password'
