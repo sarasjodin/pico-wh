ff9cece56df859a57d29a47a046efe6f887ae0aa
La svedese <5554930+sarasjodin@users.noreply.github.com>
2024-08-14 12:40:34 +0200

    Create robust.py

---
+++ 002_wifi/dht22_mcu-81/adafruit/lib/umqtt/robust.py
@@ -0,0 +1,4 @@
+# Download the MQTT Libraries required for the project and place them within the lib / umqtt libraries.
+
+# - https://github.com/micropython/micropython-lib/blob/master/umqtt.simple/umqtt/simple.py
+# - https://github.com/micropython/micropython-lib/blob/master/umqtt.robust/umqtt/robust.py
